05196e83d3ee385b9e057578ec74bb3b594e555c
Luke Tankey <luke.tankey@gmail.com>
2018-09-07 11:37:36 +1000

    Add babel transpiling for IE-arrow-function support

--- cert_client/webpack.config.js
+++ cert_client/webpack.config.js
@@ -27,6 +27,15 @@ module.exports = {
       }, {
         loader: 'sass-loader'
       }]
+    }, {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
     }]
   },
 
